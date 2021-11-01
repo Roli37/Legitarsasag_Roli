301abf228720296d2a2e06c9528b01d1f6cf9f6c
Roli37 <nyari.roland.david@njit.hu>
2021-11-01 18:12:19 +0100

    LTT2-6

---
+++ Legitarsasag_Roli/Legitarsasag_Roli/Services/LegitarsasagRepository.cs
@@ -0,0 +1,17 @@
+﻿using Legitarsasag_Roli.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Legitarsasag_Roli.Services
+{
+    public class LegitarsasagRepository : ILegitarsasagRepository
+    {
+        private readonly LegitarsasagContext _legitarsasagContext;
+        public LegitarsasagRepository(LegitarsasagContext context)
+        {
+            _legitarsasagContext = context ?? throw new ArgumentNullException(nameof(context));
+        }
+    }
+}
