32469d8946f16762f6c659b2a4314ddd5b3beb4f
Roli37 <nyari.roland.david@njit.hu>
2021-11-01 17:59:05 +0100

    LTT2-9

--- Legitarsasag_Roli/Legitarsasag_Roli/DbContexts/LegitarsasagContext.cs
+++ Legitarsasag_Roli/Legitarsasag_Roli/DbContexts/LegitarsasagContext.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Legitarsasag_Roli.DbContexts
 {
@@ -15,5 +16,41 @@ namespace Legitarsasag_Roli.DbContexts
         public DbSet<Airline> Airlines { get; set; }
         public DbSet<City> Cities { get; set; }
         public DbSet<Flight> Flights { get; set; }
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            foreach (string sor in File.ReadAllLines("input_flight.txt"))
+            {
+                int n = 0;
+                n += 1;
+                string[] s = sor.Split(';');
+                modelBuilder.Entity<Flight>().HasData(new Flight
+                {
+                    Id = n,
+                    Airline = s[0],
+                    CityFrom = s[1],
+                    CityTo = s[2],
+                    Distance = int.Parse(s[3]),
+                    Duration = int.Parse(s[4])
+                });
+                modelBuilder.Entity<Airline>().HasData(new Airline
+                {
+                    Id = n,
+                    Name = s[0]
+                });
+            }
+            foreach (string sor in File.ReadAllLines("input_city.txt"))
+            {
+                int n = 0;
+                n += 1;
+                string[] s = sor.Split(';');
+                modelBuilder.Entity<City>().HasData(new City
+                {
+                    Id = n,
+                    Name = s[0],
+                    Population = int.Parse(s[1])
+                });
+                base.OnModelCreating(modelBuilder);
+            }
+        }
     }
 }
