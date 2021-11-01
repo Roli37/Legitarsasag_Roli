aa434ca122ab1a78c71c3f5a842ee5eeae5f7549
Roli37 <nyari.roland.david@njit.hu>
2021-11-01 17:13:19 +0100

    Tablak letrehozasa

---
+++ Legitarsasag_Roli/Legitarsasag_Roli/DbContexts/LegitarsasagContext.cs
@@ -0,0 +1,19 @@
+﻿using Legitarsasag_Roli.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Legitarsasag_Roli.DbContexts
+{
+    public class LegitarsasagContext : DbContext
+    {
+        public LegitarsasagContext(DbContextOptions<LegitarsasagContext> options) : base(options)
+        {
+        }
+        public DbSet<Airline> Airlines { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
