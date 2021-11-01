55963420d57082c73754fe13486c5342565fb5e1
Roli37 <nyari.roland.david@njit.hu>
2021-11-01 16:11:40 +0100

    LTT2-3

---
+++ Legitarsasag_Roli/Legitarsasag_Roli/Entities/Flight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Legitarsasag_Roli.Entities
+{
+    public class Flight
+    {
+        public string Name { get; set; }
+        public string CityFrom { get; set; }
+        public string CityTo { get; set; }
+        public int Distance { get; set; }
+        public int Duration { get; set; }
+    }
+}
